f7bf49a2ee3949b143b8e13b0e53e237764b8fcf
orc-tzan <>
2021-11-10 15:25:22 -0300

    migrations farm manager owner

--- migrations/19_setup_arcadia_treasure_hunt.js
+++ migrations/19_setup_arcadia_treasure_hunt.js
@@ -12,9 +12,9 @@ let treasureHuntSetup;
 const contracts = require('../data/contracts');
 
 module.exports = async (deployer, network, accounts) => {
-  // if (process.env.SKIP_MIGRATION === 'true') {
-  //   return;
-  // }
+  if (process.env.SKIP_MIGRATION === 'true') {
+    return;
+  }
 
   const getContracts = contracts(network);
 
---
+++ migrations/21_setup_farm_manager_owner.js
@@ -0,0 +1,12 @@
+const APWarsFarmManagerOwner = artifacts.require("APWarsFarmManagerOwner");
+
+module.exports = async (deployer, network, accounts) => {
+  // if (process.env.SKIP_MIGRATION === 'true') {
+  //   return;
+  // }
+
+  const farmManagerOwner = await APWarsFarmManagerOwner.new();
+
+  console.log('farmManagerOwner', farmManagerOwner.address);
+  console.log("finished");
+};
