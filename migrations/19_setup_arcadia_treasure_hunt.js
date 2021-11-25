ee7efe0947ff53dc0d9ae888bd5706b1835cc384
orc-tzan <>
2021-11-25 11:40:26 -0300

    setup treasureHunt

--- migrations/19_setup_arcadia_treasure_hunt.js
+++ migrations/19_setup_arcadia_treasure_hunt.js
@@ -19,9 +19,9 @@ module.exports = async (deployer, network, accounts) => {
   const getContracts = contracts(network);
 
   const blockNumber = await web3.eth.getBlockNumber();
-  treasureHunt = await APWarsTreasureHunt.at('0x07ebef3047bb83d6ca6e3a0f921cbccc89c01b42');
-  treasureHuntEventHandler = await APWarsTreasureHuntEventHandler.at('0x4c54ae58b91a5a5d07a48930d0e3de4e0c0baff2');
-  treasureHuntSetup = await APWarsTreasureHuntSetup.at('0x5f735e8148D4CbBf7933B18bCB91657482EdB566');
+  treasureHunt = await APWarsTreasureHunt.new();
+  treasureHuntEventHandler = await APWarsTreasureHuntEventHandler.new();
+  treasureHuntSetup = await APWarsTreasureHuntSetup.new();
 
   const tokenTransfer = await APWarsTokenTransfer.at(getContracts.APWarsTokenTransfer);
   const transfer = await APWarsCollectiblesTransfer.at(getContracts.APWarsCollectiblesTransfer);
@@ -49,9 +49,8 @@ module.exports = async (deployer, network, accounts) => {
   console.log(`treasureHunt: ${treasureHunt.address}`);
   const endTreasureHunt = 57600;
 
-  // await treasureHunt.setup(treasureHuntEventHandler.address);
-  await treasureHunt.updateTreasureHunt(
-    1,
+  await treasureHunt.setup(treasureHuntEventHandler.address);
+  await treasureHunt.addTreasureHunt(
     1,
     parseInt(blockNumber.toString()) + endTreasureHunt,
     treasureHuntSetup.address,
